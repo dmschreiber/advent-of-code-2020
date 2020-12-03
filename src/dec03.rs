103271e90d0a8bc97c1519e524af9f883b928711
Dan Schreiber <dmschreiber@gmail.com>
2020-12-03 10:40:58 -0500

    day 3 parts 1 and 2

---
+++ src/dec03.rs
@@ -0,0 +1,107 @@
+use std::collections::HashMap;
+use std::fs;
+
+pub fn display_map(map: HashMap<(u32,u32),u8>) {
+  let mut max_row = 0;
+  let mut max_col = 0;
+
+  for k in map.keys() {
+    let (a,b) = k;
+    if *a > max_row { max_row = *a; }
+    if *b > max_col { max_col = *b; }
+  }
+  for row in 0..=max_row {
+    for col in 0..=max_col {
+      let b = map.get(&(row,col));
+      if let Some(i) = b {
+        print!("{}", *i as char);
+      }
+    }
+    println!();
+  }
+}
+
+pub fn display_tree_map(map: &HashMap<(u32,u32),u8>) {
+  let mut max_row = 0;
+  let mut max_col = 0;
+
+  for k in map.keys() 
+  {
+    let (a,b) = k;
+    if *a > max_row { max_row = *a; }
+    if *b > max_col { max_col = *b; }
+  }
+  for row in 0..=max_row 
+  {
+    for col in 0..=max_col 
+    {
+      let b = map.get(&(row,col));
+      if let Some(i) = b {
+        print!("{}", *i as char);
+      }
+    }
+    println!();
+  }
+}
+
+pub fn read_tree_map(filename: String) -> HashMap<(u32,u32),u8> {
+  let mut map = HashMap::new();
+
+  let mut col = 0;
+  let mut row = 0;
+
+  let contents = fs::read_to_string(filename)
+  .expect("Something went wrong reading the file");
+  
+  let bytes = contents.as_bytes();
+  for &item in bytes {
+      if item == 10 {
+        row += 1;
+        col = 0;
+      } else {
+        map.entry((row,col)).or_insert(item);
+        col += 1;
+      }
+  }
+
+  map
+}  
+
+pub fn solve(map: &HashMap<(u32,u32),u8>, row_step: u32, col_step: u32) -> u32 {
+  let mut max_row = 0;
+  let mut max_col = 0;
+
+  for k in map.keys() 
+  {
+    let (a,b) = k;
+
+    if *a > max_row { max_row = *a; }
+    if *b > max_col { max_col = *b; }
+  }
+
+  println!("amx {},{}", max_row, max_col);
+
+  let mut col = 0;
+  let mut tree_count = 0;
+  for row in (row_step..=max_row).step_by(row_step as usize)
+  {
+    col = col + col_step;
+    col = col % (max_col + 1);
+
+    let b = map.get(&(row,col));
+      if let Some(i) = b {
+        if *i == 35 {
+          // tree
+          tree_count = tree_count + 1;
+          // println!("Tree {},{} - {}", row, col, *i as char);
+        } else if *i == 46 {
+          // blank
+          // println!("No Tree {},{} - {}", row, col, *i as char);
+        } else {
+          println!("Problem");
+        }
+      }
+  }
+  println!("tree count {}", tree_count);
+  tree_count
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::time::{Instant};
 mod dec01;
 mod dec02;
+mod dec03;
 
 fn dec01() {
     let mut my_numbers = dec01::read_input("/Users/dschreiber/Projects/advent-of-code-2020/src/dec01.txt".to_string());
@@ -22,6 +23,21 @@ fn dec02() {
     dec02::solve_part2(&my_passwords);
 }
 
+fn dec03() {
+    let my_tree_map = dec03::read_tree_map("/Users/dschreiber/Projects/advent-of-code-2020/src/dec03.txt".to_string());
+
+    let start = Instant::now();
+    let p = 
+    dec03::solve(&my_tree_map,1,1) as u128 *
+    dec03::solve(&my_tree_map,1,3)  as u128*
+    dec03::solve(&my_tree_map,1,5) as u128*
+    dec03::solve(&my_tree_map,1,7) as u128*
+    dec03::solve(&my_tree_map,2,1) as u128;
+    println!("{} is the product of all (4239209520 is wrong)", p);
+    println!("Day 3 complete in {:?}", start.elapsed());
+
+}
+
 fn main() {
     let start = Instant::now();
     dec01();
@@ -31,4 +47,5 @@ fn main() {
     dec02();
     println!("Day 2 complete in {:?}", start.elapsed());
 
+    dec03();
 }
