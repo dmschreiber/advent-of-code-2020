dd87a40c6c5e47d5122e3c3a6377d3b2a46fb089
Dan Schreiber <dmschreiber@gmail.com>
2020-12-07 15:15:53 -0500

    use hashmap for faster indexing

--- src/dec07.rs
+++ src/dec07.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::collections::HashMap;
+
 
 const GOLD : &str = "shiny gold";
 
@@ -13,8 +15,8 @@ pub fn read_input(filename: String) -> Vec<String> {
   rules
 }
 
-pub fn create_structure(rule_lines: &Vec<String>) -> Vec<BagRule> {
-  let mut rules = <Vec<BagRule>>::new();
+pub fn create_structure(rule_lines: &Vec<String>) -> HashMap<String,BagRule> {
+  let mut rules = <HashMap<String,BagRule>>::new();
   for r in rule_lines {
 
     let parts = r.split("contain").collect::<Vec<&str>>();
@@ -22,7 +24,7 @@ pub fn create_structure(rule_lines: &Vec<String>) -> Vec<BagRule> {
     let containing_bag = get_bag_name(parts[0].to_string());
     let contained_bags = parts[1].split(", ").map(|s| (&*s).to_string() ).collect();
     
-    rules.push(BagRule{ contained_bag: containing_bag, bags_contained: contained_bags, 
+    rules.insert (containing_bag.clone(), BagRule{ contained_bag: containing_bag.to_string(), bags_contained: contained_bags, 
         has_gold: parts[1].find(GOLD) != None,
         has_no_other_bags: parts[1].find("no other bags") != None });
 
@@ -69,10 +71,14 @@ fn get_bag_name(s: String) -> String {
   }
 }
 
-fn does_contain(rules: &Vec<BagRule>, which: String) -> u32 {
-  for rule in rules {
-    if rule.contained_bag == which {
+fn does_contain(rules: &HashMap<String,BagRule>, which: String) -> u32 {
 
+  let rule;
+  if let Some(i) = rules.get(&which) {
+    rule = i;
+  } else {
+    return 0;
+  }
       if rule.has_gold {
         return 1;
       } else {
@@ -87,53 +93,49 @@ fn does_contain(rules: &Vec<BagRule>, which: String) -> u32 {
 
       }
       return 0;
-    }
-  }
-  return 0;
 }
 
-pub fn solve(rules: &Vec<BagRule>) -> u32 {
+pub fn solve(rules: &HashMap<String,BagRule>) -> u32 {
 
-  let bags = rules.iter().map(|rule| does_contain(rules, rule.contained_bag.to_string())).sum::<u32>();
+  let bags = rules.values().map(|rule| does_contain(rules, rule.contained_bag.to_string())).sum::<u32>();
 
   println!("{} bags can contain shiny gold", bags);
   return bags;
 }
 
-fn how_many_bags(rules: &Vec<BagRule>, which: &String, how_many: u32) -> u32 {
+fn how_many_bags(rules: &HashMap<String,BagRule>, which: &String, how_many: u32) -> u32 {
   
-  for rule in rules {
+  let rule;
+  if let Some(i) = rules.get(which) {
+    rule = i;
+  } else {
+    return 0;
+  }
 
-    if rule.contained_bag == *which {
-      // println!("{}working {}x{}", level,how_many,containing_bag);
+  if rule.has_no_other_bags {
+    // println!("{}{} has no other bags returning {}", level, parts[0].to_string(), how_many);
+    return how_many;
+  } else {
 
-      if rule.has_no_other_bags {
-        // println!("{}{} has no other bags returning {}", level, parts[0].to_string(), how_many);
-        return how_many;
-      } else {
+    let sub_bag_count = rule.bags_contained.iter().map(
+      |bag| 
+      how_many_bags(rules, &get_bag_name(bag.to_string()), get_bag_count(bag.to_string()))
+        ).sum::<u32>();
 
-        let sub_bag_count = rule.bags_contained.iter().map(
-          |bag| 
-          how_many_bags(rules, &get_bag_name(bag.to_string()), get_bag_count(bag.to_string()))
-            ).sum::<u32>();
-
-        let ret_val = how_many * sub_bag_count + how_many;
-        
-        // println!("{}done {} returning {}", level, containing_bag, ret_val);
-        if rule.contained_bag == GOLD {
-          return ret_val - 1;
-        } else {
-          return ret_val;
-        }
-      }
+    let ret_val = how_many * sub_bag_count + how_many;
+    
+    // println!("{}done {} returning {}", level, containing_bag, ret_val);
+    if rule.contained_bag == GOLD {
+      return ret_val - 1;
+    } else {
+      return ret_val;
     }
   }
-  return 0;
 }
 
-pub fn solve_part2(rules: &Vec<BagRule>) -> u32 {
+pub fn solve_part2(rules: &HashMap<String,BagRule>) -> u32 {
 
-  let answer = how_many_bags(rules, &GOLD.to_string(), 1);
+  let answer = how_many_bags(&rules, &GOLD.to_string(), 1);
   println!("shiny gold has {} bags", answer);
   return answer;
 
