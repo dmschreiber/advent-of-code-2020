e439f9e9f8bfc45510a25a3742f2f03c887534a9
Dan Schreiber <dmschreiber@gmail.com>
2020-12-12 10:01:33 -0500

    simplifying the code

--- src/dec12.rs
+++ src/dec12.rs
@@ -5,7 +5,7 @@ pub struct Thing {
   operator: Operator,
   argument: i32,
 }
-#[derive(Debug)]
+#[derive(Debug,Copy,Clone)]
 enum Operator {
   North,
   South,
@@ -15,98 +15,8 @@ enum Operator {
   Right,
   Forward,
 }
+// ********** COMMON ***************
 
-// ********** PART 1 ***************
-// ********** PART 1 ***************
-// ********** PART 1 ***************
-
-fn turn_right(op: Operator, degrees: i32) -> Operator {
-  match op {
-    Operator::North => {
-      match degrees {
-        90 => { Operator::East }
-        180 => { Operator::South }
-        270 => { Operator::West }
-        360 => { Operator::North }
-        _ => {panic!("failed left turn");}
-      }
-    }
-    Operator::East => {
-      match degrees {
-        90 => { Operator::South }
-        180 => { Operator::West }
-        270 => { Operator::North }
-        360 => { Operator::East }
-        _ => {panic!("failed left turn");}
-      }
-    }
-    Operator::South => {
-      match degrees {
-        90 => { Operator::West }
-        180 => { Operator::North }
-        270 => { Operator::East }
-        360 => { Operator::East }
-        _ => {panic!("failed left turn");}
-      }
-
-    }
-    Operator::West => {
-      match degrees {
-        90 => { Operator::North }
-        180 => { Operator::East }
-        270 => { Operator::South }
-        360 => { Operator::West }
-        _ => {panic!("failed left turn");}
-      }
-
-    }
-    _ => {panic!("failed"); }
-  }
-}
-
-fn turn_left(op: Operator, degrees: i32) -> Operator {
-  match op {
-    Operator::North => {
-      match degrees {
-        90 => { Operator::West }
-        180 => { Operator::South }
-        270 => { Operator::East }
-        360 => { Operator::North }
-        _ => {panic!("failed left turn");}
-      }
-    }
-    Operator::East => {
-      match degrees {
-        90 => { Operator::North }
-        180 => { Operator::West }
-        270 => { Operator::South }
-        360 => { Operator::East }
-        _ => {panic!("failed left turn");}
-      }
-    }
-    Operator::South => {
-      match degrees {
-        90 => { Operator::East }
-        180 => { Operator::North }
-        270 => { Operator::West }
-        360 => { Operator::East }
-        _ => {panic!("failed left turn");}
-      }
-
-    }
-    Operator::West => {
-      match degrees {
-        90 => { Operator::South }
-        180 => { Operator::East }
-        270 => { Operator::North }
-        360 => { Operator::West }
-        _ => {panic!("failed left turn");}
-      }
-
-    }
-    _ => {panic!("failed"); }
-  }
-}
 pub fn solve() {
   let lines: Vec<String> = include_str!("../inputs/dec12.txt").lines().map(|s| (&*s).to_string() ).collect();
   let mut things = <Vec<Thing>>::new();
@@ -126,16 +36,48 @@ pub fn solve() {
       _ => panic!("Invalid char"),
     };
 
-
     things.push( Thing {argument: arg.parse::<i32>().unwrap(), operator: operator } );
   }
 
   // solve logic
-  let mut facing = Operator::East;
-  let mut x = 0;
-  let mut y = 0;
+  solve_part1(&things);
+  solve_part2(&things);
+}
 
-  for t in &things {
+// ********** PART 1 ***************
+// ********** PART 1 ***************
+// ********** PART 1 ***************
+fn move_part1(direction:Operator, units: i32, pos: (i32,i32)) -> (i32,i32) {
+  let new_x;
+  let new_y;
+
+    match direction {
+    Operator::North => {
+      new_y = pos.1 + units;
+      new_x = pos.0;
+    }
+    Operator::East => {
+      new_x = pos.0 + units;
+      new_y = pos.1;
+    }
+    Operator::South => {
+      new_y = pos.1 - units;
+      new_x = pos.0;
+    }
+    Operator::West => {
+      new_x = pos.0- units;
+      new_y = pos.1;
+    }
+    _ => {panic!("invalid direction"); }
+  }
+  (new_x,new_y)
+}
+
+fn solve_part1(things: &Vec<Thing>) {
+  let mut facing = Operator::East;
+  let mut position = (0,0);
+
+  for t in things {
     match t.operator {
       Operator::Left => {
         facing = turn_left(facing,t.argument);
@@ -143,115 +85,105 @@ pub fn solve() {
       Operator::Right => {
         facing = turn_right(facing,t.argument);
       }
-      Operator::North => {
-        y = y + t.argument;
-      }
-      Operator::East => {
-        x = x + t.argument;
-      }
-      Operator::South => {
-        y = y - t.argument;
-      }
-      Operator::West => {
-        x = x - t.argument;
+      Operator::North | Operator::East | Operator::West | Operator::South => {
+        position = move_part1(t.operator, t.argument, position);
       }
       Operator::Forward => {
-        match facing {
-          Operator::North => {
-            y = y + t.argument;
-          }
-          Operator::East => {
-            x = x + t.argument;
-          }
-          Operator::South => {
-            y = y - t.argument;
-          }
-          Operator::West => {
-            x = x - t.argument;
-          }
-          _ => { panic!("fail");}
-        }
+        position = move_part1(facing, t.argument, position);
       }
     }
   }
-  println!("Day 12 Part 1 manhattan is {}", x.abs() + y.abs());
-  solve_part2(&things);
+  println!("Day 12 Part 1 manhattan is {}", (position.0 as i32).abs() + (position.1 as i32).abs());
+}
+
+fn turn_right(op: Operator, degrees: i32) -> Operator {
+  match degrees {
+    90 => {   
+      match op {
+        Operator::North => { Operator::East }
+        Operator::East => { Operator::South }
+        Operator::South => { Operator::West }
+        Operator::West => { Operator::North }
+        _ => {panic!("failed turn"); }
+      }
+    }
+    180 => { turn_right(turn_right(op,90),90) }
+    270 => { turn_right(turn_right(turn_right(op,90),90),90) }
+    360 => { op }
+    _ => {panic!("failed left turn");}
+  }
+}
+
+fn turn_left(op: Operator, degrees: i32) -> Operator {
+  match degrees {
+    90 => {   
+      match op {
+        Operator::North => { Operator::West }
+        Operator::West => { Operator::South }
+        Operator::South => { Operator::East }
+        Operator::East => { Operator::North }
+        _ => {panic!("failed turn"); }
+      }
+    }
+    180 => { turn_left(turn_left(op,90),90) }
+    270 => { turn_left(turn_left(turn_left(op,90),90),90) }
+    360 => { op }
+    _ => {panic!("failed left turn");}
+  }
 }
 
 // ********** PART 2 ***************
 // ********** PART 2 ***************
 // ********** PART 2 ***************
-fn turn_left_waypoint(units_x : i32, units_y :i32, degrees : i32) -> (i32,i32) {
-  let new_x;
-  let new_y;
+fn turn_left_waypoint(position: (i32,i32), degrees : i32) -> (i32,i32) {
+  let new_pos;
 
   match degrees {
-      270 => { new_x = units_y; new_y = -1 * units_x; }
-      180 => { new_x = -1 * units_x; new_y = -1 * units_y; }
-      90 => { new_x = -1 * units_y; new_y = units_x; }
-      360 => { panic!("360");}
-      _ => {panic!("failed left turn");}
+    90 => { new_pos = turn_right_waypoint(turn_right_waypoint(turn_right_waypoint(position,90),90),90); }
+    180 => { new_pos = turn_left_waypoint(turn_left_waypoint(position,90),90); }
+    270 => { new_pos = turn_right_waypoint(position,90); }
+    360 => { new_pos = position; }
+    _ => {panic!("failed left turn");}
   }
 
-
-  (new_x,new_y)
+  new_pos
 }
 
-fn turn_right_waypoint(units_x : i32, units_y :i32, degrees : i32) -> (i32,i32) {
-  let new_x;
-  let new_y;
+fn turn_right_waypoint(position: (i32,i32), degrees : i32) -> (i32,i32) {
+  let new_pos;
 
   match degrees {
-    90 => { new_x = units_y; new_y = -1 * units_x; }
-    180 => { new_x = -1 * units_x; new_y = -1 * units_y; }
-    270 => { new_x = -1 * units_y; new_y = units_x; }
+    90 => { new_pos = (position.1, -1 * position.0); }
+    180 => { new_pos = turn_right_waypoint(turn_right_waypoint(position,90),90); }
+    270 => { new_pos = turn_right_waypoint(turn_right_waypoint(turn_right_waypoint(position,90),90),90); }
     360 => { panic!("360"); }
     _ => {panic!("failed left turn");}
   }
 
-  
-  (new_x,new_y)
+  new_pos
 }
 
 fn solve_part2(things: &Vec<Thing>) {
-
-  let mut units_x = 10;
-  let mut units_y = 1;
-
-  let mut x = 0;
-  let mut y = 0;
+  let mut waypoint = (10,1);
+  let mut position = (0,0);
 
   for t in things {
     match t.operator {
       Operator::Left => {
-        let a = turn_left_waypoint(units_x,units_y,t.argument);
-        units_x = a.0;
-        units_y = a.1;
+        waypoint = turn_left_waypoint(waypoint,t.argument);
       }
       Operator::Right => {
-        let a = turn_right_waypoint(units_x,units_y,t.argument);
-        units_x = a.0;
-        units_y = a.1;
+        waypoint = turn_right_waypoint(waypoint,t.argument);
       }
-      Operator::North => {
-        units_y = units_y + t.argument;
-      }
-      Operator::East => {
-        units_x = units_x + t.argument;
-      }
-      Operator::South => {
-        units_y = units_y - t.argument;
-      }
-      Operator::West => {
-        units_x = units_x - t.argument;
+      Operator::North | Operator::East | Operator::South | Operator::West => {
+        waypoint = move_part1(t.operator, t.argument, waypoint);
       }
       Operator::Forward => {
-        x = x + units_x * t.argument;
-        y = y + units_y * t.argument;
+        position = (position.0 + waypoint.0 * t.argument, position.1 + waypoint.1 * t.argument);
 
       }
     }
   }
-  println!("Day 12 Part 2 manhattan is {}", x.abs() + y.abs());
+  println!("Day 12 Part 2 manhattan is {}", (position.0 as i32).abs() + (position.1 as i32).abs());
 
 }
