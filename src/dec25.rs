4d1a035520f3c965cf3489ae46a57fd600efbf08
Dan Schreiber <dmschreiber@gmail.com>
2020-12-25 14:10:24 -0500

    fixup ways

--- src/dec25.rs
+++ src/dec25.rs
@@ -61,19 +61,27 @@ fn another_way(key : u32) -> u32 {
 fn find_loop_size_from_key(key : u32) -> u32 {
   let mut loop_size = 1;
 
+  let mut acc = 1;
+  let mut loop_count = 1;
   loop {
-    println!("{:8}", (0..loop_size).fold(1, |acc,_n| (acc * 7) % SECRET));
-    if (0..loop_size).fold(1, |acc,_n| (acc * 7) % SECRET) == key {
-      println!("{:8}", (0..loop_size+1).fold(1, |acc,_n| (acc * 7) % SECRET));
-      return loop_size;
-    }
-    loop_size = loop_size + 1;
+    acc = (acc * 7 ) % SECRET;
+    if acc == key { return loop_count; }
+    loop_count = loop_count + 1;
   }
+
+  // loop {
+  //   println!("{:8}", (0..loop_size).fold(1, |acc,_n| (acc * 7) % SECRET));
+  //   if (0..loop_size).fold(1, |acc,_n| (acc * 7) % SECRET) == key {
+  //     println!("{:8}", (0..loop_size+1).fold(1, |acc,_n| (acc * 7) % SECRET));
+  //     return loop_size;
+  //   }
+  //   loop_size = loop_size + 1;
+  // }
 }
 
 pub fn solve_part1(key1 : u32, key2 : u32) -> u128 {
 
-  let loop_size1 = another_way(key1);
+  let loop_size1 = find_loop_size_from_key(key1);
   let loop_size2 = another_way(key2);
   let public_key1 = (0..loop_size1).fold(1, |acc,_n| (acc * 7) % SECRET);
   assert!(public_key1 == key1);
